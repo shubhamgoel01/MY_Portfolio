79967df8c45a23933882a555de5bb187b3eb6337
Shubham <thorgoyal@gmail.com>
2021-04-09 13:34:14 +0530

    file added

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title    
+
+    
+      
+    
+
